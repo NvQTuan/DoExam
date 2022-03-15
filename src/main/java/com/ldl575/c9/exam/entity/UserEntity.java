56144e234654bdd2a3fb6488db2dee051214e717
NVQ Tuan <nvquoctuan@gmail.com>
2022-03-15 21:57:22 +0700

    Add user entity

--- src/main/java/com/ldl575/c9/exam/entity/UserEntity.java
+++ src/main/java/com/ldl575/c9/exam/entity/UserEntity.java
@@ -1,10 +1,15 @@
 package com.ldl575.c9.exam.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -24,6 +29,19 @@ public class UserEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
 	private int id;
-	
 	private String userName;
+	private String fullName;
+	
+	private String unit;
+	
+	@Column(length = 50)
+	private String password;
+	private String role;
+	private String mititaryRank;
+	
+	@Temporal(TemporalType.DATE)
+	private Date createdAt;
+	
+	@Temporal(TemporalType.DATE)
+	private Date lastestUpdate;
 }
